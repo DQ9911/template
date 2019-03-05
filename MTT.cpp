b392f9675c3c56438c2f14672a86cee766843489
DQ9911 <43775609+DQ9911@users.noreply.github.com>
2019-03-05 22:47:07 +0800

    Update MTT.cpp

--- MTT.cpp
+++ MTT.cpp
@@ -1,3 +1,4 @@
+// luogu-judger-enable-o2
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -24,20 +25,19 @@ namespace FASTIO {
 using namespace FASTIO;
 struct cpd {
     db x, y;
-    cpd(db _x=0, db _y=0):x(_x),y(_y){}
-    cpd &operator *= (cpd &R) { return *this = cpd{x*R.x-y*R.y, x*R.y+y*R.x};}
-    cpd &operator /= (db t) { return *this = cpd{x/t, y/t}; }
-    cpd operator + (cpd R) { return cpd{x + R.x, y + R.y}; }
-    cpd operator - (cpd R) { return cpd{x - R.x, y - R.y}; }
+    cpd(db _x = 0, db _y = 0):x(_x), y(_y){}
+    cpd &operator *= (cpd &R) { return *this = cpd(x*R.x-y*R.y, x*R.y+y*R.x);}
+    cpd &operator /= (db t) { return *this = cpd(x/t, y/t); }
+    cpd operator + (cpd R) { return cpd(x + R.x, y + R.y); }
+    cpd operator - (cpd R) { return cpd(x - R.x, y - R.y); }
     cpd operator * (cpd R) { cpd ret = *this; return ret *= R; }
-    friend cpd conj(cpd R) { return cpd{R.x, -R.y}; }
-    bool operator == (cpd R) { return x == R.x && y == R.y;}
+    friend cpd conj(cpd R) { return cpd(R.x, -R.y); }
 };
-cpd x[N], y[N], w[N];
 
+cpd x[N], y[N], w[N];
 void fft(cpd *y, int len, int on) {
-    static int r[N], nl;
     static cpd u, v, t;
+    static int r[N], nl;
     int i, j, k, l = __builtin_ctz(len);
     if(nl != l) for(nl = l, i = 0; i < len; i++)
         r[i] = (r[i>>1]>>1)|((i&1)<<l-1);
@@ -53,29 +53,36 @@ void fft(cpd *y, int len, int on) {
     }
     if(on) for(i = 0; i < len; i++) y[i] /= len;
 }
-int t[4][N], res[4][N], f[N], g[N], h[N], n, m, p;
-void conv(int *a, int *b, int *c, int len) {
-    int i, j;
-    for(i = 0; i < len; i++) x[i] = cpd{a[i], b[i]};
-    fft(x, len, 0);
-    for(i = j = 0; i < len; i++, j = len - i)
-        y[i] = (x[i] * x[i] - conj(x[j] * x[j])) * cpd{0, -0.25};
-    fft(y, len, 1);
-    for(i = 0; i < len; i++) c[i] = ll(y[i].x + 0.5) % p;
-}
 
+int f[N], g[N], h[N], n, m, p;
 void solve(int *a, int *b, int *c, int l1, int l2) {
+    static cpd dft[4][N];
+    static int i, j, d[4];
     int len = l1 + l2 - 1;
     while(len & (len - 1)) len += len & -len;
-    for(int i = 0; i < len; i++) {
-        t[0][i] = a[i] & M, t[1][i] = a[i] >> 15;
-        t[2][i] = b[i] & M, t[3][i] = b[i] >> 15;
+    for(i = 0; i < len; i++) {
+        x[i] = cpd(a[i] & M, a[i] >> 15);
+        y[i] = cpd(b[i] & M, b[i] >> 15);
     }
-    conv(t[0], t[2], res[0], len), conv(t[0], t[3], res[1], len);
-    conv(t[1], t[2], res[2], len), conv(t[1], t[3], res[3], len);
-    for(int i = 0; i < len; i++) {
-        c[i] = ((1ll<<30) * res[3][i] % p + res[0][i]) % p;
-        (c[i] += (1ll<<15) * (res[1][i] + res[2][i]) % p) %= p;
+    fft(x, len, 0), fft(y, len, 0);
+    const cpd half = cpd(0.5, 0);
+    for(i = j = 0; i < len; i++, j = len - i) {
+        dft[0][i] = x[i] * y[i] * half;
+        dft[1][i] = conj(x[j]) * y[i] * half;
+        dft[2][i] = x[i] * conj(y[j]) * half;
+        dft[3][i] = conj(x[j]) * conj(y[j]) * half;
+    }
+    for(i = 0; i < len; i++) {
+        x[i] = dft[0][i] + dft[1][i];
+        y[i] = dft[2][i] - dft[3][i];
+    }
+    fft(x, len, 1), fft(y, len, 1);
+
+    for(i = 0; i < len; i++) {
+        d[0] = ll(x[i].x + 0.5) % p, d[1] = ll(x[i].y + 0.5) % p;
+        d[2] = ll(y[i].y + 0.5) % p, d[3] = ll(y[i].x + 0.5) % p;
+        c[i] = ((1ll<<30) * d[3] % p + d[0]) % p;
+        c[i] = (c[i] + (1ll<<15) * (d[1] + d[2]) % p) % p;
     }
 }
 
@@ -84,7 +91,7 @@ int main() {
     freopen("in.txt", "r", stdin);
 #endif
     for(int i = 0; i <= all; i++)
-        w[i] = cpd{cos(i*pi/all), sin(i*pi/all)};
+        w[i] = cpd(cos(i*pi/all), sin(i*pi/all));
     scan(n), scan(m), scan(p);
     n++, m++;
     for(int i = 0; i < n; i++) scan(f[i]);
